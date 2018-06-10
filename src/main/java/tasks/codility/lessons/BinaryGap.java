e2421184571469ff042d02fbd020ec55bc96d494
Numichi <csdonat1@gmail.com>
2018-06-10 15:49:41 +0000

    BinaryGap

---
+++ src/main/java/tasks/codility/lessons/BinaryGap.java
@@ -0,0 +1,21 @@
+package tasks.codility.lessons;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Codility Lesson #1
+ */
+class BinaryGap {
+    int solution(int N) {
+        int max = 0;
+
+        Matcher matcher = Pattern.compile("(?<=1)0+(?=1)").matcher(Integer.toBinaryString(N));
+        while (matcher.find()) {
+            int temp = matcher.group().length();
+            max = temp > max ? temp : max;
+        }
+
+        return max;
+    }
+}
---
+++ src/test/java/tasks/codility/lessons/BinaryGapTest.java
@@ -0,0 +1,34 @@
+package tasks.codility.lessons;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BinaryGapTest {
+    private BinaryGap binaryGap;
+
+    @BeforeClass
+    public void setUpBeforeClass() {
+        binaryGap = new BinaryGap();
+    }
+
+    @DataProvider(name = "default")
+    public static Object[][] dataProvider() {
+        return new Object[][] {
+            {9, 2},
+            {529, 4},
+            {1041, 5},
+            {32, 0},
+            {328, 2},
+        };
+    }
+
+    @Test(dataProvider = "default")
+    public void test(int input, int expected) {
+        Assert.assertEquals(binaryGap.solution(input), expected, "Input was: " + input);
+    }
+}
